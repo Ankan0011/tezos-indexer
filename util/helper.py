dc13093efb9944b35a9b902d6bbf55e0ab540bc2
Ankan Ghosh <ankan.ghosh@uzh.ch>
2022-09-30 14:51:43 +0000

    Graph Update

--- util/helper.py
+++ util/helper.py
@@ -27,19 +27,34 @@ def initalizeSpark(AppName):
 def initalizeGraphSpark(AppName):
         print("Importing the Graph Initialization")
         spark = SparkSession.builder.appName(AppName) \
+                .config("spark.driver.extraJavaOptions", "-Xss400M") \
                 .config("spark.jars", "//mnt/indexer-build/jar/postgresql-42.5.0.jar") \
                 .config("spark.driver.extraClassPath","//mnt/indexer-build/jar/graphframes_graphframes-0.8.1-spark3.0-s_2.12.jar") \
-                .config("spark.executor.memory", "12g") \
-                .config("spark.executor.cores", "3") \
-                .config('spark.cores.max', '6') \
-                .config('spark.driver.memory','4g') \
+                .config("spark.executor.memory", "8g") \
+                .config("spark.executor.cores", "2") \
+                .config('spark.cores.max', '4') \
+                .config('spark.driver.memory','16g') \
+                .config('spark.executor.instances','3') \
+                .config('spark.dynamicAllocation.enabled', 'true') \
+                .config('spark.dynamicAllocation.shuffleTracking.enabled', 'true') \
+                .config('spark.dynamicAllocation.executorIdleTimeout', '60s')\
+                .config('spark.dynamicAllocation.minExecutors', '0')\
+                .config('spark.dynamicAllocation.maxExecutors', '5')\
+                .config('spark.dynamicAllocation.initialExecutors', '1')\
+                .config('spark.dynamicAllocation.executorAllocationRatio', '1')\
                 .config('spark.worker.cleanup.enabled', 'true') \
                 .config('spark.worker.cleanup.interval', '60') \
                 .config('spark.shuffle.service.db.enabled', 'true') \
                 .config('spark.worker.cleanup.appDataTtl', '60') \
+                .config('spark.excludeOnFailure.killExcludedExecutors','true') \
                 .config('spark.sql.debug.maxToStringFields', 100).getOrCreate()
         spark.sparkContext.setCheckpointDir(dirName="/mnt/indexer-build/migrated_data/temp")
+        # spark.sparkContext.setSystemProperty('logger.File', '/mnt/indexer-build/migrated_data/logs/sample.log')
+        # logger = spark._jvm.org.apache.log4j.LogManager.getLogger('default')
+
         return spark
+
+
 
 def loadFile(spark, path, header):
         print("Loading the csv files")
