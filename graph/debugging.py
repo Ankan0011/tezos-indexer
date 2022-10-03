fc76f8359a1b6271de437d8dd9770020eec8d1f8
Ankan Ghosh <ankan.ghosh@uzh.ch>
2022-10-03 13:44:00 +0000

    networkx implementation for SCC.

--- graph/debugging.py
+++ graph/debugging.py
@@ -1,7 +1,8 @@
-from pyspark.sql.functions import *
+from pyspark.sql.functions import lit
 from util.helper import initalizeGraphSpark,loadFile
 from graphframes import *
 import os
+import networkx as nx
 
 # Paths
 test_txs_path="/mnt/indexer-build/migrated_data/stage/SSC"
@@ -14,6 +15,7 @@ spark = initalizeGraphSpark("Debug")
 v1_cols=["Id","Balance"]
 e1_cols=["SenderId","TargetId","Year_no","Week_no"]
 e1_final=["src","dst","relationship"]
+final_columns = ["max_count","time_week","Total_Vertices"]
 
 df_accounts = loadFile(spark, accounts_path, True )
 v1 = df_accounts.select(v1_cols).withColumnRenamed("Id","id")
@@ -23,21 +25,25 @@ listDesDir =[x[0].split("/")[-1] for x in os.walk(destination_path)]
 listSrcDir  = [x[0] for x in os.walk(test_txs_path)]
 
 for x in listSrcDir:
-    if x.find("relationship=202152")  != -1:
+    if x.find("relationship=2021")  != -1:
         dirname = x.split("/")[-1]
-        #if not (dirname in listDesDir):
-            #print("Not Found it :"+dirname)
-        df_new = loadFile(spark, x, True )
-        e1 = df_new.groupBy("src","dst").count().withColumn("relationship",lit('txs')).select(e1_final)
-        try:
-            g = GraphFrame(v1,e1).dropIsolatedVertices()
-            result = g.stronglyConnectedComponents(maxIter=10)
-            final = result.select("id", "component").groupBy("component").agg(countDistinct(result["id"]).alias("count"))
-            next = final.agg(max("count").alias("max_count")).withColumn("time_week",lit(dirname.split("=")[-1])).withColumn("Total_Vertices",lit(g.vertices.count()))
-            next.show()
-            #next.write.option("header", True).mode('overwrite').csv(destination_path+"/"+dirname)
-        except NameError:
-            print("Exception")
-            print(NameError)
+        if not (dirname in listDesDir):
+            print("Not Found it :"+dirname)
+            df_new = loadFile(spark, x, True )
+            e1 = df_new.groupBy("src","dst").count().withColumn("relationship",lit('txs')).select(e1_final).toPandas()
+            try:
+                G = nx.from_pandas_edgelist(e1, "src", "dst", create_using=nx.DiGraph())
+                largest = len(max(nx.strongly_connected_components(G), key=len))
+                data = [(largest, dirname.split("=")[-1], len(G.nodes))]
+                # g = GraphFrame(v1,e1).dropIsolatedVertices()
+                # result = g.stronglyConnectedComponents(maxIter=10)
+                # final = result.select("id", "component").groupBy("component").agg(countDistinct(result["id"]).alias("count"))
+                # next = final.agg(max("count").alias("max_count")).withColumn("time_week",lit(dirname.split("=")[-1])).withColumn("Total_Vertices",lit(g.vertices.count()))
+                next = spark.createDataFrame(data).toDF(*final_columns)
+                next.show()
+                # next.write.option("header", True).mode('overwrite').csv(destination_path+"/"+dirname)
+            except NameError:
+                print("Exception")
+                print(NameError)
 
 spark.stop()
